70b5e9086d9e6181219aeaaad4f8fd3a39d5cb69
Josleee <cz2f@qq.com>
2019-01-17 16:51:37 +0100

    One hot vector initialization

--- scripts/outcome_process.py
+++ scripts/outcome_process.py
@@ -8,3 +8,18 @@ outcome[outcome == 8] = 3
 outcome[outcome == 16] = 4
 
 np.save('outcome_ohv.npy', outcome)
+
+new_outcome = np.zeros(((outcome.shape[0]), 5))
+for i in range(outcome.shape[0]):
+    if outcome[i] == 0:
+        new_outcome[i, 0] = 1
+    elif outcome[i] == 1:
+        new_outcome[i, 1] = 1
+    elif outcome[i] == 2:
+        new_outcome[i, 2] = 1
+    elif outcome[i] == 3:
+        new_outcome[i, 3] = 1
+    elif outcome[i] == 4:
+        new_outcome[i, 4] = 1
+
+np.save('outcome_ohv_r5.npy', outcome)
