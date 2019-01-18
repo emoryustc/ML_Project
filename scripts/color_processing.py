d5a1ac8f72f48ecf797d3d8ecb509bc9371da917
Josleee <cz2f@qq.com>
2019-01-18 16:26:55 +0100

    Add feature extraction function

--- scripts/color_processing.py
+++ scripts/color_processing.py
@@ -16,8 +16,9 @@ unique_color_set = ['White', 'Red', 'Pink', 'Silver', 'Flame', 'Blue', 'Liver', 
 
 unique_color_set_for_dog = ['Pink', 'Silver', 'Ruddy', 'Blue', 'Tan', 'Liver', 'White', 'Sable', 'Orange', 'Gray',
                             'Chocolate', 'Yellow', 'Fawn', 'Brown', 'Apricot', 'Cream', 'Buff', 'Gold', 'Black', 'Red']
-unique_color_group_for_dog = ['light', 'light', 'medium', 'medium', 'dark', 'dark', 'light', 'dark', 'medium', 'medium',
-                              'dark', 'light', 'light', 'dark', 'light', 'light', 'light', 'light', 'dark', 'medium']
+unique_color_group_for_dog = ['light', 'light', 'l-medium', 'l-medium', 'dark', 'dark', 'light', 'dark', 'l-medium',
+                              'l-medium',
+                              'dark', 'light', 'light', 'dark', 'light', 'light', 'light', 'light', 'dark', 'l-medium']
 unique_color_group2_for_dog = ['warm', 'medium', 'warm', 'cold', 'medium', 'medium', 'medium', 'medium', 'warm',
                                'medium', 'medium', 'warm', 'warm', 'medium', 'warm', 'medium', 'warm', 'warm', 'medium',
                                'warm']
@@ -96,41 +97,70 @@ def get_pattern(color):
     return pattern
 
 
-if __name__ == '__main__':
+def get_and_set_param_by_color_theory(color_des):
+    """
+    Set the color relevant parameters
+    If animal has multi colors, all of them are taken into consideration
+    - Light / Medium / Dark
+    - Warm / Medium / Cold
+
+    :param color_des:       color description
+    :return:
+    """
+
     color_group_1 = np.array([unique_color_set_for_dog, unique_color_group_for_dog]).T
-    dog_group_1 = np.unique(color_group_1[:, 1])
+    # dog_group_1 = np.unique(color_group_1[:, 1])
 
     color_group_2 = np.array([unique_color_set_for_dog, unique_color_group2_for_dog]).T
-    dog_group_2 = np.unique(color_group_2[:, 1])
+    # dog_group_2 = np.unique(color_group_2[:, 1])
 
-    # if breed in dog_group_1[:, 0]:
-    #     if breed in dog_group_2[:, 0]:
+    list_feature = set()
+    for item in color_des.split('/'):
+        for des in item.split(' '):
+            if des in color_group_1[:, 0]:
+                ind1 = np.where(color_group_1[:, 0] == des)[0]
+                # print(des)
+                # print(color_group_1[ind1, 1][0])
+                ind2 = np.where(color_group_2[:, 0] == des)[0]
+                # print(color_group_2[ind2, 1][0])
+                # print()
+                list_feature.add(color_group_1[ind1, 1][0])
+                list_feature.add(color_group_2[ind2, 1][0])
 
+    # print(list_feature)
+    return list_feature
+
+
+if __name__ == '__main__':
     colorset = list(get_colorset())
     dataset = np.load('dataset.npy')
-    print(dataset[0])
+    dataset = np.hstack((dataset, np.zeros((dataset.shape[0], 6), dtype=dataset.dtype)))
 
     for i in df.index:
-        # get color number
-        colornumber = get_color(df.at[i, 'Color'])
-        # print(colornumber)
+        get_and_set_param_by_color_theory(df.at[i, 'Color'])
 
-        if colornumber == 0:
-            df.at[i, 'ColorCategory'] = "unknow"
-        elif colornumber == 1:
-            df.at[i, 'ColorCategory'] = "unicolor"
-        elif colornumber == 2:
-            df.at[i, 'ColorCategory'] = "two-tones"
-        elif colornumber == 3:
-            df.at[i, 'ColorCategory'] = "tricolor"
+    #     # get color number
+    #     colornumber = get_color(df.at[i, 'Color'])
+    #     # print(colornumber)
+    #
+    #     if colornumber == 0:
+    #         df.at[i, 'ColorCategory'] = "unknow"
+    #     elif colornumber == 1:
+    #         df.at[i, 'ColorCategory'] = "unicolor"
+    #     elif colornumber == 2:
+    #         df.at[i, 'ColorCategory'] = "two-tones"
+    #     elif colornumber == 3:
+    #         df.at[i, 'ColorCategory'] = "tricolor"
+    #
+    #     # print('Colornumber', df.at[i, 'ColorCategory'], "color", df.at[i, 'Color'])
+    #
+    #     # get patterns
+    #     pattern = get_pattern(df.at[i, 'Color'])
+    #     # print('color',df.at[i, 'Color'],'pattern',pattern)
+    #     df.at[i, 'Pattern'] = pattern
+    #     # if '/' in pattern:
+    #     # mixedlist.append(pattern)
+    #     # df['Pattern'] = "mixed"
+    #     # print('new',df['Pattern'])
 
-        # print('Colornumber', df.at[i, 'ColorCategory'], "color", df.at[i, 'Color'])
-
-        # get patterns
-        pattern = get_pattern(df.at[i, 'Color'])
-        # print('color',df.at[i, 'Color'],'pattern',pattern)
-        df.at[i, 'Pattern'] = pattern
-        # if '/' in pattern:
-        # mixedlist.append(pattern)
-        # df['Pattern'] = "mixed"
-        # print('new',df['Pattern'])
+    np.save('dataset_color6.npy', dataset)
